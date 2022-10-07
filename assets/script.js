9f0adf469c549b2a0c61057d3abe4cd624b56e1a
teedaa <pengtedachengllhs@gmail.com>
2022-10-06 23:26:08 -0700

    Fix color coding

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 //variables
 
 var currentDate = moment().format("[Today is] LL");
-var currentTime = moment().format("LTS");
 var currentDay = $("#currentDay");
-var saveBtn = $(".saveBtn")
+var saveBtn = $(".saveBtn");
+
 
 $(document).ready(function (){
     
@@ -15,11 +15,45 @@ currentDay.append(currentDate);
 saveBtn.on("click", function(event){
   event.preventDefault();
   var text = $(this).siblings(".description").val();
-  var time = $(this).parent().attr("id");
+  var time = $(this).siblings("id").val();
     localStorage.setItem(time, text);
 })
 
+//display schedule
 
+$("#hour-9 .description").val(localStorage.getItem("9"));
+$("#hour-10 .description").val(localStorage.getItem("10"));
+$("#hour-11 .description").val(localStorage.getItem("11"));
+$("#hour-12 .description").val(localStorage.getItem("12"));
+$("#hour-13 .description").val(localStorage.getItem("13"));
+$("#hour-14 .description").val(localStorage.getItem("14"));
+$("#hour-15 .description").val(localStorage.getItem("15"));
+$("#hour-16 .description").val(localStorage.getItem("16"));
+$("#hour-17 .description").val(localStorage.getItem("17"));
+
+
+//color code
+function colorCodeTime() {
+    $('.description').each(function () {
+        var currentTime = parseInt(moment().format("H"));
+        var timeBlock = parseInt((this).id);
+console.log(currentTime)
+console.log(timeBlock)
+        if (currentTime < timeBlock) {
+            $(this).addClass("future")
+            // .removeClass("past present");
+        }
+        else if (currentTime > timeBlock) {
+            $(this).addClass("past")
+            // .removeClass("present future");
+        }
+        else if (currentTime === timeBlock) {
+            $(this).addClass("present")
+            // .removeClass("past future")
+        }
+    });
+}
+colorCodeTime();
 })
 
 
