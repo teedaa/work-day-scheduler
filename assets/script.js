60a1c782422f74388aee2457adc5540784abe7b1
teedaa <pengtedachengllhs@gmail.com>
2022-10-06 21:37:11 -0700

    Display workday

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,47 @@
 //variables
 
-var currentDate = moment().format("[Today is] dddd, MMM, Do YYYY");
+var currentDate = moment().format("[Today is] LL");
 console.log(currentDate);
-var currentTime = moment().hour();
+var currentTime = moment().format("LTS");
+console.log(currentTime)
 var saveBtn = $(".saveBtn");
 var currentDay = $("#currentDay");
 
 
-//display current date
 $(document).ready(function (){
     
+//display current date
 currentDay.append(currentDate);
+
+//save current schedule text to local storage
+
+saveBtn.on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+    
+})
+// display saved schedule from local storage
+localStorage.getItem(time,text);
+
+
+//color code
+var colorCodeTime = function() {
+    $(".hour").each(function(){
+        var hourSection = parseInt($(this).attr("id"));
+
+        if (time > hourSection){
+            $(this).siblings("description").addClass("past");
+        } 
+        else if (time < hourSection){
+            $(this).siblings("description").addClass("future");
+        } 
+        else {
+            $(this).siblings("description").addClass("present");
+        }
+    });
+};
+
 })
 
 
