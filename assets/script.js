54eb2fe3f195639aac00fc0d0a02b7664c1f5796
teedaa <pengtedachengllhs@gmail.com>
2022-10-06 21:55:04 -0700

    Fix last hour class redo JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,9 @@
 //variables
 
 var currentDate = moment().format("[Today is] LL");
-console.log(currentDate);
 var currentTime = moment().format("LTS");
-console.log(currentTime)
-var saveBtn = $(".saveBtn");
 var currentDay = $("#currentDay");
-
+var saveBtn = $(".saveBtn")
 
 $(document).ready(function (){
     
@@ -15,32 +12,13 @@ currentDay.append(currentDate);
 
 //save current schedule text to local storage
 
-saveBtn.on("click", function(){
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+saveBtn.on("click", function(event){
+  event.preventDefault();
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
-    
 })
-// display saved schedule from local storage
-localStorage.getItem(time,text);
 
-
-//color code
-var colorCodeTime = function() {
-    $(".hour").each(function(){
-        var hourSection = parseInt($(this).attr("id"));
-
-        if (time > hourSection){
-            $(this).siblings("description").addClass("past");
-        } 
-        else if (time < hourSection){
-            $(this).siblings("description").addClass("future");
-        } 
-        else {
-            $(this).siblings("description").addClass("present");
-        }
-    });
-};
 
 })
 
